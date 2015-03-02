2c6ca233938a9637ecf3a6ee70de9d0e9f9dbe86
Tilen Popek <tilen.popek95@gmail.com>
2015-03-02 09:43:24 +0100

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
+  #resources :comments
 
-  resources :posts
+  resources :posts do
+    resources :comments, :only => [:create]
+  end
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
